7a80c8a93ddf333b1d9fc75ee0b6d38686c8d063
Fred Ramos <fred.ramos@capgemini.com>
2021-02-09 13:49:58 -0600

    accding ability to connect to Snowflake and doing a simple query

--- webserver.py
+++ webserver.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from snowflake import connector
+import pandas as pd
 
 app = Flask("my website")
 
 @app.route('/')
 def homepage():
-    return 'Welcome to my website!'
+    return 'Welcome to my website! My Snowflake Acct # is: ' + str(onerow)
+
+# Snowflake
+cnx = connector.connect(
+    account='BLANK',
+    user='BLANK',
+    password='BLANK'
+)
+cur = cnx.cursor()
+cur.execute('SELECT current_account()')
+onerow = cur.fetchone()
 
 app.run()
