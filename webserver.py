69ea9d059e4cdfca4e59afe79d80d37e3c123b9a
Fred Ramos <fred.ramos@capgemini.com>
2021-02-09 14:54:20 -0600

    Adding index template and turning Snowbase returned query into dataframe then into an HTML table

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from snowflake import connector
 import pandas as pd
 
@@ -6,16 +6,24 @@ app = Flask("my website")
 
 @app.route('/')
 def homepage():
-    return 'Welcome to my website! My Snowflake Acct # is: ' + str(onerow)
+    return render_template('index.html', colors_table = dfhtml)
 
 # Snowflake
 cnx = connector.connect(
     account='BLANK',
     user='BLANK',
-    password='BLANK'
+    password='BLANK',
+    warehouse='COMPUTE_WH',
+    database='DEMO_DB',
+    schema='PUBLIC'
 )
 cur = cnx.cursor()
-cur.execute('SELECT current_account()')
-onerow = cur.fetchone()
+cur.execute('SELECT * FROM COLORS')
+rows=pd.DataFrame(cur.fetchall(),columns=['Color UID', 'Color Name'])
+# print(rows)
+
+# test dataframe as html, built-in method for pandas dataframe
+dfhtml = rows.to_html()
+print(dfhtml)
 
 app.run()
