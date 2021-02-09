4caae04b08d50ca2fe9af646585c719ba62a3991
Fred Ramos <fred.ramos@capgemini.com>
2021-02-09 17:17:08 -0600

    letting users input their favorite colors with form submission and confirmation routes

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from snowflake import connector
 import pandas as pd
 
@@ -7,6 +7,18 @@ app = Flask("my website")
 @app.route('/')
 def homepage():
     return render_template('index.html', colors_table = dfhtml)
+
+@app.route('/submit')
+def submitpage():
+    return render_template('submit.html')
+
+@app.route('/thanks4submit', methods=['POST'])
+def thanks4submit():
+    colorname = request.form.get('cname')
+    username = request.form.get('uname')
+    return render_template('thanks4submit.html'
+                        , colorname=colorname
+                        , username=username)
 
 # Snowflake
 cnx = connector.connect(
@@ -20,10 +32,8 @@ cnx = connector.connect(
 cur = cnx.cursor()
 cur.execute('SELECT * FROM COLORS')
 rows=pd.DataFrame(cur.fetchall(),columns=['Color UID', 'Color Name'])
-# print(rows)
 
-# test dataframe as html, built-in method for pandas dataframe
+# dataframe as html, built-in method for pandas dataframe
 dfhtml = rows.to_html()
-print(dfhtml)
 
 app.run()
